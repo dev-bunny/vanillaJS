10dd3737070e99641d31fb77051dc2abebfb54dd
dev-bynny <eugene.devdev@gmail.com>
2020-12-14 18:58:05 +0900

    manage localstorage and add functions

--- clock.js
+++ clock.js
@@ -1,21 +1,21 @@
-const clockContainer = document.querySelector('.js-clock'),
-  clockTitle = clockContainer.querySelector('h1');
+const clockContainer = document.querySelector('.js-clock');
+const clockTitle = clockContainer.querySelector('h1');
 
 function getTime() {
-  const date = new Date();
-  const minutes = date.getMinutes();
-  const hours = date.getHours();
-  const seconds = date.getSeconds();
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
 
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+    }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
-  const interval = 1000;
+    const interval = 1000;
 
-  setInterval(getTime, interval);
+    setInterval(getTime, interval);
 }
 
 init();
